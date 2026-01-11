c6e3f46945dc49fbe2839b8ded44f56efd174c0d
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2026-01-12 01:04:22 +0530

    product deatails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,31 @@
 import React, { useContext } from 'react'
 import { ProductDataContext } from '../Context/ProductContext'
+import { useParams } from 'react-router-dom'
 
-const { productData } = useContext(ProductDataContext)
 
 const ProductDetails = () => {
-    return (
-        <div>ProductDetails
+    
+const [productData] = useContext(ProductDataContext)
 
+const {id} = useParams()
+
+const SelectedProduct = productData.find((item)=> item.id === id)
+
+console.log(id)
+
+    return (
+        <div> 
+            ProductDetails
+            {productData.map((item,index)=>{
+                return(
+                    <div key={index}>
+                        <img src={item.image} alt={item.title} />
+                        <h1>{item.title}</h1>
+                        <p>{item.price}</p>
+                        <p>{item.description}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
